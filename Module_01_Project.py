6bb81c487ca879eff3b40f57ba3cf92b8fd0a4a3
Pranay Ashok Ramteke <69661033+Phoenix600@users.noreply.github.com>
2022-07-10 13:11:27 -0700

    Create Module_01_Project.py

---
+++ Module_01_Project.py
@@ -0,0 +1,11 @@
+# Pranay Ramteke
+# I did try , to make this code under one variable
+name = '''
+ pppp   RRRRR         A       N           N       A         N           N    Y        Y
+p    P  R    R      A   A     N N         N     A   A       N N         N      Y     Y  
+p    P  R    R     A      A   N   N       N    A      A     N   N       N       Y   Y
+PPPPPP  RRRRRR     AAAAAAAA   N     N     N    AAAAAAAA     N     N     N         Y
+p       RR        A        A  N       N   N   A         A   N       N   N         Y
+p       R R       A        A  N         N N   A         A   N         N N         Y
+p       R   R     A        A  N           N   A         A   N           N         Y
+'''
